7e4322614f46506fface8cff05f21d27aab008cc
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2022-09-03 14:17:17 -0300

    :sparkles:feat: add interface IEmployee

---
+++ src/Models/Employee.ts
@@ -0,0 +1,11 @@
+export interface IEmployee {
+  id: string,
+  name: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  profession: string;
+  dataHiring: Date;
+  salary: string;
+}
+
