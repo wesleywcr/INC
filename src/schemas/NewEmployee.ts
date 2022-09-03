8b22306becbb56cef16f8d43fa0e26c65d053c50
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2022-09-03 14:21:52 -0300

    :sparkles:feat: add lastName

--- src/schemas/NewEmployee.ts
+++ src/schemas/NewEmployee.ts
@@ -1,18 +1,16 @@
 import * as Yup from 'yup';
 export const AddNewEmployeeShema = Yup.object().shape({
   name: Yup.string().required('Nome é obrigatório'),
+  lastName: Yup.string().required('Sobrenome é obrigatório'),
   email: Yup.string()
     .email('O campo deve conter um e-mail valido')
     .required('Email é uma campo obrigatório'),
   phone: Yup.string()
     .min(11, `O Telefone deve conter no mínimo 11 caracteres`)
     .required('Telefone é um campo obrigatório'),
-  dataBirth: Yup.date()
-    .max(new Date(), 'Não é possível incluir uma data futura')
-    .required('Data de nascimento é um campo obrigatório '),
+  profession: Yup.string().required('Cargo é um campo obrigatório'),
   dataHiring: Yup.date()
-    .max(new Date(), 'Não é possível incluir uma data futura')
-    .required('Data de nascimento é um campo obrigatório '),
+    .required('Data de contratação é um campo obrigatório ').nullable(),
   salary: Yup.string()
     .required('Salário é um campo obrigatório')
 
